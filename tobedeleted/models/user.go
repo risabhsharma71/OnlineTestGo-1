4d1c43bcf97e8f0eb7e3092f901cea100c8a2395
sa_admin <sa_admin@corp.rpqb.com>
2017-01-24 14:38:23 +0530

    models

--- tobedeleted/models/user.go
+++ tobedeleted/models/user.go
@@ -1,17 +1,13 @@
 package models 
  
  
- type ( 
- 	User struct { 
- 		Registration_id     int   	 `json:"Registration_id" bson:"_Registration_id"` 
- 		First_Name   	 	string	 `json:"First_Name" bson:"First_Name"` 
-		Last_Name           string 	 `json:"Last_Name" bson:"Last_Name"`
-		Phone_no            int    	 `json:"Phone_no" bson:"Phone_no"`  
- 		Email_id            string 	 `json:"Email_id" bson:"Email_id"` 
- 		
- 	} 
- ) 
- 
+var(
+		Registration_id int
+		First_Name string
+		Last_name string
+		Phone_no int
+		Email_id string
+	)
 
 
 
