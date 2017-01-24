59719966c5d9f66e3865a9e7991fb44e02f2e80b
sa_admin <sa_admin@corp.rpqb.com>
2017-01-24 14:11:03 +0530

    mysqlconn

---
+++ tobedeleted/daos/mysqlconn.go
@@ -0,0 +1,20 @@
+package main 
+import(
+//"fmt"
+"log"
+"database/sql"
+ _"github.com/go-sql-driver/mysql"
+)
+
+ 
+func main(){
+ 	db, err := sql.Open("mysql",
+			"rakesh:root@tcp(192.168.0.8:3306)/interview_test1")
+	if err != nil {		
+			log.Fatal(err)
+	}
+	insertlist(db)
+	getlist(db)
+	defer db.Close()
+
+}
