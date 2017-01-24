fa2f8abe4929861b47f9650fea85d845c7a5cbd1
sa_admin <sa_admin@corp.rpqb.com>
2017-01-24 14:21:16 +0530

    userinput

---
+++ tobedeleted/daos/userinput.go
@@ -0,0 +1,39 @@
+package daos
+import(
+"database/sql"
+ _"github.com/go-sql-driver/mysql"
+)
+
+ func insertlist(db *sql.DB)	{
+rows, err := db.Query("insert into registration_table(Registration_id,First_Name,Last_name,Phone_no,Email_id) values(120,'Rakesh1','Bharati','9681817926','rakesh.bharati@rapidqube.com')")
+if err != nil {
+	log.Fatal(err)
+	}
+defer rows.Close()
+err = rows.Err()
+}
+func getlist(db *sql.DB){
+var(
+		Registration_id int
+		First_Name string
+		Last_name string
+		Phone_no int
+		Email_id string
+	)
+rows, err := db.Query("select * from registration_table")
+if err != nil {
+	log.Fatal(err)
+}
+defer rows.Close()
+for rows.Next() {
+	err := rows.Scan(&Registration_id, &First_Name, &Last_name, &Phone_no, &Email_id)
+	if err != nil { 
+		log.Fatal(err)
+	}
+	log.Println(Registration_id, First_Name, Last_name, Phone_no, Email_id)
+}
+err = rows.Err()
+if err != nil {
+		log.Fatal(err)
+}
+}
