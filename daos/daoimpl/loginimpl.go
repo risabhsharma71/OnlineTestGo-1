8478d469b3d4d86fe7c1b66ae44ea17088c01979
dhananjay <dhananjay@ip-172-31-23-46.us-west-2.compute.internal>
2017-03-17 16:48:52 +0000

    pull

---
+++ daos/daoimpl/loginimpl.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "time"
+
+    "github.com/dgrijalva/jwt-go"
+)
+
+func (dao LoginImpl) SaveNewUser(login models.Login) (int64, error) {
+
+	query := "insert into registration(email,password) values(?,?)"
+	db := connection()
+	defer db.Close()
+
+	stmt, err := db.Prepare(query)
+
+	if err != nil {
+	return 0, err
+	}
+
+func ( []byte) (string, error) {
+    // Create the token
+    token := jwt.New(jwt.SigningMethodHS256)
+    // Set some claims
+    token.Claims["foo"] = "bar"
+    token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+    // Sign and get the complete encoded token as a string
+    tokenString, err := token.SignedString(mySigningKey)
+    return tokenString, err
+}
+
+func ExampleParse(myToken string, myKey string) {
+    token, err := jwt.Parse(myToken, func(token *jwt.Token) (interface{}, error) {
+        return []byte(myKey), nil
+    })
+
+    if err == nil && token.Valid {
+        fmt.Println("Your token is valid.  I like your style.")
+    } else {
+        fmt.Println("This token is terrible!  I cannot accept this.")
+    }
+}
--- webservice/Login.go
+++ webservice/Login.go
@@ -11,12 +11,18 @@ func Login(c *gin.Context) {
 	var login models.Login
 	var login1 models.Login
 
+
+	login1:= c.Query("email/fname")
+	login1:=c.Query("password")
+
+	login1 := manager.FetchUser(email/fname)
+
 	c.BindJSON(&login)
 
-	if login.Email == "testuser" {
-		login1 = models.Login{Uid: 10, Fname: "dj", Token: "ghctfdtf", UserType: "admin"}
-	}
-        login1 := manager.Login(email)
+	//if login.Email == "testuser@gmail.com" {
+	//	login1 = models.Login{Uid: 10, Fname: "dj", Token: "ghctfdtf", UserType: "admin"}
+	//}
+        	login1 := manager.Login(email)
 	//function should be calling this manager class
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
