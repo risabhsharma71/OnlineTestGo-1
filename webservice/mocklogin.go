c0e8519cbbf711a1246e3b32a1ba55cea7999c41
umashankar somasekar <umashankar somasekar>
2017-03-18 11:28:37 +0530

    mock ligin

--- webservice/mocklogin.go
+++ webservice/mocklogin.go
@@ -8,26 +8,23 @@ import (
 
 func Mocklogin(c *gin.Context) {
 
-	var login models.Login
+	var user models.User
 	var login1 models.Login
-	
+
+	c.BindJSON(&user)
+
+	msgstr := "failure"
 	if user.Email == "test@t.com" {
-	login1 = models.Login{Uid: 10, Fname: "dj", Token: "ghctfdtf", UserType: "user"}
-		
+		login1 = models.Login{Uid: 10, Fname: "dj", Token: "ghctfdtf", UserType: "user"}
+		msgstr = "success"
+
 	}
-
-	c.BindJSON(&login)
-
-	//if user.Email == "test@t.com" {
-	//	login1 = models.Login{Uid: 10, Fname: "dj", Token: "ghctfdtf", UserType: "user"}
-		
-	//}
 
 	//function should be calling this manager class
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
 	c.JSON(200, gin.H{
-		"status":  Success,
+		"status":  msgstr,
 		"message": login1,
 	})
 }
