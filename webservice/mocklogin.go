dad07b0c99a70fbaa0a445744d4d033ce07eccc7
dhananjay <dhananjay@ip-172-31-23-46.us-west-2.compute.internal>
2017-03-18 05:40:19 +0000

    mocklogin

--- webservice/mocklogin.go
+++ webservice/mocklogin.go
@@ -16,14 +16,14 @@ func Mocklogin(c *gin.Context) {
 
 	if user.Email == "test@t.com" {
 		login1 = models.Login{Uid: 10, Fname: "dj", Token: "ghctfdtf", UserType: "user"}
-		message = "success"
+		
 	}
 
 	//function should be calling this manager class
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
 	c.JSON(200, gin.H{
-		"status":  message,
+		"status":  Success,
 		"message": login1,
 	})
 }
